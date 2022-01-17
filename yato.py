73df9812726b0e51f569e6067663166ade592ba8
progamer9138 <39977408+progamer9138@users.noreply.github.com>
2022-01-16 16:40:32 -0800

    restructure

--- yato.py
+++ yato.py
@@ -1,56 +1,69 @@
 import sys
 
-if sys.argv[1] == '-h' or sys.argv[1] == '--help': #help at help.txt
+def help():
     helpFile = open('help.txt', 'r')
     print(helpFile.read())
     helpFile.close()
-    sys.exit()
-elif sys.argv[1] == '-n' or sys.argv[1] == '--new': #New List
-    try:
-        fileLocation = sys.argv[2]
-        open(fileLocation, 'x')
-        print('File created at: ' + fileLocation)
-    except IndexError: #if no file location is given
-        print('No file location given.  Example: /home/user/yato.txt')
-        sys.exit()
-    except FileExistsError: #if file already exists
-        print('File already exists.')
-        sys.exit()
-    except PermissionError: #if file location is not writable
-        print('Permission denied.')
-        sys.exit()
-elif sys.argv[1] == '-a' or sys.argv[1] == '--add': #Add to list
-    try:
-        fileLocation = sys.argv[2]
-        file = open(fileLocation, 'a')
+
+def new(fileLocation):
+    print('File created at: ' + fileLocation)
+    
+def addToList(list, add):
+    file = open(list, 'a')
+    file.write(add + '\n')
+    print(f'Task {sys.argv[3]} added.')
+    file.close()
+
+def listTasks(fileLocation):
+    file = open(fileLocation, 'r')
+    print(file.read())
+    file.close()
+
+def main():
+    print(sys.argv[1])
+    if sys.argv[1] == '-h' or sys.argv[1] == '--help': #help at help.txt
+        help()
+    elif sys.argv[1] == '-n' or sys.argv[1] == '--new': #New List
         try:
-            file.write(sys.argv[3] + '\n')
-        except IndexError: #if no task is given
-            print('No task given.')
+            new(sys.argv[2])
+        except IndexError: #if no file location is given
+            print('No file location given.  Example: /home/user/yato.txt')
             sys.exit()
-        print(f'Task {sys.argv[3]} added.')
-        file.close()
-    except FileNotFoundError:
-        print('File not found.')
-        sys.exit()
-    except PermissionError:
-        print('Permission denied.')
-        sys.exit()
-    except IndexError:
-        print('No file location given.')
-        sys.exit()
-elif sys.argv[1] == '-l' or sys.argv[1] == '--list': #List all tasks
-    try:
-        fileLocation = sys.argv[2]
-        file = open(fileLocation, 'r')
-        print(file.read())
-        file.close()
-    except FileNotFoundError:
-        print('File not found.')
-        sys.exit()
-    except PermissionError:
-        print('Permission denied.')
-        sys.exit()
-    except IndexError:
-        print('No file location given.')
-        sys.exit()
+        except FileExistsError: #if file already exists
+            print('File already exists.')
+            sys.exit()
+        except PermissionError: #if file location is not writable
+            print('Permission denied.')
+            sys.exit()
+    elif sys.argv[1] == '-a' or sys.argv[1] == '--add': #Add to list
+        try:
+            fileLocation = sys.argv[2]
+            try:
+                add = sys.argv[3]
+                addToList(fileLocation, add)
+            except:
+                print('No task given.')
+                sys.exit()
+        except FileNotFoundError:
+            print('File not found.')
+            sys.exit()
+        except PermissionError:
+            print('Permission denied.')
+            sys.exit()
+        except IndexError:
+            print('No file location given.')
+            sys.exit()
+    elif sys.argv[1] == '-l' or sys.argv[1] == '--list': #List all tasks
+        try:
+            listTasks(sys.argv[2])
+        except FileNotFoundError:
+            print('File not found.')
+            sys.exit()
+        except PermissionError:
+            print('Permission denied.')
+            sys.exit()
+        except IndexError:
+            print('No file location given.')
+            sys.exit()
+            
+main()
