301c3dbc4f3caed7a6a2cff6ec232faf4ae0f3e6
progamer9138 <39977408+progamer9138@users.noreply.github.com>
2022-01-16 19:14:21 -0800

    mark as complete/incomplete

--- actions.py
+++ actions.py
@@ -1,3 +1,5 @@
+from termcolor import colored
+
 def help():
     helpFile = open('help.txt', 'r')
     print(helpFile.read())
@@ -8,11 +10,36 @@ def new(fileLocation):
     
 def addToList(list, add):
     file = open(list, 'a')
-    file.write(add + '\n')
+    file.write(add + ',incomplete' + '\n')
     print(f'Task {add} added.')
     file.close()
 
 def listTasks(fileLocation):
     file = open(fileLocation, 'r')
-    print(file.read())
+    lines = file.readlines()
+    for line in reversed(lines):
+        if line.split(',')[1] == 'completed\n':
+            print(colored(line.split(',')[0], 'green'))
+        else:
+            print(colored(line.split(',')[0], 'red'))
     file.close()
+
+def completeTask(list, task):
+    file = open(list, 'r')
+    lines = file.readlines()
+    file.close()
+    file = open(list, 'w')
+    for line in lines:
+        if line == task + ',incomplete\n':
+            line = task+',completed\n'
+            file.write(line)
+            file.close()
+            print(f'Task {task} completed.')
+            return
+        elif line == task + ',completed\n':
+            line = task+',incomplete\n'
+            file.write(line)
+            file.close()
+            print(f'Task {task} uncompleted.')
+            return
+    print(f'Task {task} not found.')
--- yato.py
+++ yato.py
@@ -7,9 +7,10 @@ def main():
     elif getCmd() == '-n' or getCmd() == '--new': #New List
         new(getFile(2)) #getFile(2) is the file location
     elif getCmd() == '-a' or getCmd() == '--add': #Add to list
-        fileLocation = getFile(2)
-        addToList(fileLocation, getTask(3))
+        addToList(getFile(2), getTask(3))
     elif getCmd() == '-l' or getCmd() == '--list': #List all tasks
         listTasks(getFile(2))
+    elif getCmd() == '-c' or getCmd() == '--complete': #Complete task
+        completeTask(getFile(2), getTask(3))
 
 main()
