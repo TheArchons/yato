ceaa1b4559baa56f7a7a41dec572265cda6e76ae
progamer9138 <39977408+progamer9138@users.noreply.github.com>
2022-01-16 16:06:12 -0800

    Added response after file creation

--- input.py
+++ input.py
@@ -9,6 +9,13 @@ elif sys.argv[1] == '-n' or sys.argv[1] == '--new':
     try:
         fileLocation = sys.argv[2]
         open(fileLocation, 'x')
+        print('File created at: ' + fileLocation)
     except IndexError:
         print('No file location given.  Example: /home/user/TODO.txt')
+        sys.exit()
+    except FileExistsError:
+        print('File already exists.')
+        sys.exit()
+    except PermissionError:
+        print('Permission denied.')
         sys.exit()
