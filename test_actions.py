4e5eb80f7a6b463631b7da6305e5f9fcde196a9f
TheArchons <progamer9138@gmail.com>
2022-04-17 20:14:51 -0700

    update ListNameEdit tests

--- test_actions.py
+++ test_actions.py
@@ -3,6 +3,7 @@ from actions import *
 import os
 
 def test_ListNameEdit():
+    createListList()  # create lists.json if it doesn't exist
     #remove json files incase they exist
     if os.path.exists('test.json'):
         os.remove('test.json')
@@ -17,21 +18,27 @@ def test_ListNameEdit():
 
     ListNameEdit('test.json', 'test2.json')
     assert os.path.exists('test2.json')
+    assert ('tests2.json' in json.loads(open('lists.json').read())['lists']) and not ('test.json' in json.loads(open('lists.json').read())['lists'])
 
     ListNameEdit('test2.json', 'test2.json')
     assert os.path.exists('test2.json')
+    assert ('tests2.json' in json.loads(open('lists.json').read())['lists'])
 
     ListNameEdit('test2.json', 'asd.txt')
     assert os.path.exists('asd.txt')
+    assert ('asd.txt' in json.loads(open('lists.json').read())['lists']) and not ('test2.json' in json.loads(open('lists.json').read())['lists'])
 
     ListNameEdit('asd.txt', 'asd.txt')
     assert os.path.exists('asd.txt')
+    assert ('asd.txt' in json.loads(open('lists.json').read())['lists'])
 
     ListNameEdit('asd.txt', '111.json')
     assert os.path.exists('111.json')
+    assert ('111.json' in json.loads(open('lists.json').read())['lists']) and not ('asd.txt' in json.loads(open('lists.json').read())['lists'])
 
     ListNameEdit('111.json', 'test.json')
     assert os.path.exists('test.json')
+    assert ('test.json' in json.loads(open('lists.json').read())['lists']) and not ('111.json' in json.loads(open('lists.json').read())['lists'])
 
     os.remove('test.json')
 
